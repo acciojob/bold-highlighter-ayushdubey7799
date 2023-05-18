18785b8c48fd432e4b6b5df38fe9b06750853700
ayushdubey7799 <113593098+ayushdubey7799@users.noreply.github.com>
2023-05-18 18:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+const strng = document.getElementsByTagName('strong');
 function highlight() {
     //Write your code here
-
+	
+  for(x of strng){
+	  x.style.color ='green';
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+  for(x of strng){
+	  x.style.color = '';
+  }
     
 }
